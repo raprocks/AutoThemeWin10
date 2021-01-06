269f6d375d1553940371a7eb11d15a3fb7c47449
Rohit Patil <rahulhimesh09@gmail.com>
2021-01-06 15:08:15 +0530

    drastic heirarchial changes because flit + license

---
+++ autothemewin10/__init__.py
@@ -0,0 +1,8 @@
+'''
+     Automatic Theme Changer for Windows 10(1909+)
+
+    The Script Runs as Scheduled Task every Hour and Changes the Light Mode to Dark or Light based on the time. Has no Dependancies but needs typer for the CLI.
+    Using the register-scheduled-task command registers the script as the scheduled task all by itself
+    '''
+
+__version__ = "1.0.0"
--- autothemewin10/main.py
+++ autothemewin10/main.py
@@ -1,3 +1,12 @@
+'''
+     Automatic Theme Changer for Windows 10(1909+)
+
+    The Script Runs as Scheduled Task every Hour and Changes the Light Mode to Dark or Light based on the time. Has no Dependancies but needs typer for the CLI.
+    Using the register-scheduled-task command registers the script as the scheduled task all by itself
+    '''
+
+__version__ = "1.0.0"
+
 import subprocess
 from datetime import datetime
 import typer
